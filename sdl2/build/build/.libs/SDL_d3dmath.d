57b401097316c9c674491ee45b405bc046744637
Arthur WOIMBEE <awoimbee@e1r9p4.42.fr>
2019-02-01 16:39:05 +0100

    remove sdl2 binaries & artefacts

--- sdl2/build/build/.libs/SDL_d3dmath.d
+++
@@ -1,9 +0,0 @@
-build/SDL_d3dmath.lo: \
- /home/arthurw/Desktop/rt-v1/sdl2/sources/src/render/SDL_d3dmath.c \
- /home/arthurw/Desktop/rt-v1/sdl2/sources/src/render/../SDL_internal.h \
- /home/arthurw/Desktop/rt-v1/sdl2/sources/src/render/../dynapi/SDL_dynapi.h \
- /home/arthurw/Desktop/rt-v1/sdl2/sources/src/render/../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h \
- /home/arthurw/Desktop/rt-v1/sdl2/sources/include/SDL_platform.h \
- /home/arthurw/Desktop/rt-v1/sdl2/sources/include/begin_code.h \
- /home/arthurw/Desktop/rt-v1/sdl2/sources/include/close_code.h
--- sdl2/build/build/.libs/SDL_render_psp.d
+++
@@ -1,9 +0,0 @@
-build/SDL_render_psp.lo: \
- /home/arthurw/Desktop/rt-v1/sdl2/sources/src/render/psp/SDL_render_psp.c \
- /home/arthurw/Desktop/rt-v1/sdl2/sources/src/render/psp/../../SDL_internal.h \
- /home/arthurw/Desktop/rt-v1/sdl2/sources/src/render/psp/../../dynapi/SDL_dynapi.h \
- /home/arthurw/Desktop/rt-v1/sdl2/sources/src/render/psp/../../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h \
- /home/arthurw/Desktop/rt-v1/sdl2/sources/include/SDL_platform.h \
- /home/arthurw/Desktop/rt-v1/sdl2/sources/include/begin_code.h \
- /home/arthurw/Desktop/rt-v1/sdl2/sources/include/close_code.h
